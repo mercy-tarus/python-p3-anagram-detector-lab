49cc5b2406222e1815c841204a1fb2260b6cefe1
mercy-tarus <merciechelah1@gmail.com>
2024-06-06 13:37:12 +0300

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        matches = []
+        for candidate in words:
+            if self.is_anagram(candidate):
+                matches.append(candidate)
+        return matches
+
+    def is_anagram(self, candidate):
+        return sorted(self.word.lower()) == sorted(candidate.lower())
